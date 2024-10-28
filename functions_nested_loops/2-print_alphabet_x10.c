cf756f3512ef8437ad91c5a3fc35190145b1eff5
elifigueroa9725 <elijfigueroaperez@gmail.com>
2024-10-27 21:50:42 -0400

    added exercise 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - prints the alphabet ten times.
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int line;
+
+	line = 0;
+
+	while (line < 10)
+	{
+		alphabet = 'a';
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+		line++;
+	}
+}
