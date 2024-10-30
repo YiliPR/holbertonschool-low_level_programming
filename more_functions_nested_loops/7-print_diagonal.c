7191dcd3be5e135caacc00ada4ed6609a5e3bc2e
elifigueroa9725 <elijfigueroaperez@gmail.com>
2024-10-30 14:34:32 -0400

    fixed command prompt

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,13 @@ void print_diagonal(int n)
 	int i;
 	int p;
 
-	if (n < 0)
+	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (p = 0; p < i; p++)
+			for (p = 0; p <= i; p++)
 				_putchar(' ');
 		}
 		_putchar('\\');
