04ab70a636dc5da2969d27ca73458616541101c7
elifigueroa9725 <elijfigueroaperez@gmail.com>
2024-10-30 13:20:20 -0400

    fixed command prompt

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -4,19 +4,16 @@
  * print_line - prints a line a determined
  * amount of time.
  * @n: function variable.
- * @i: function variable.
  */
 
 void print_line(int n)
 {
-
 	if (n <= 0)
 		_putchar('\n');
-
 	else
 	{
 		int i;
-		
+
 		for (i = 0; i < n; i++)
 		_putchar('_');
 	_putchar('\n');
