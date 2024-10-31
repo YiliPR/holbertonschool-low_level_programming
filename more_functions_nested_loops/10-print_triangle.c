488a34013762ba58189fbc01f5828b0bbaa61581
elifigueroa9725 <elijfigueroaperez@gmail.com>
2024-10-30 22:38:04 -0400

    fixed command prompt

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle.
+ * @size: function variable
+ */
+
+void print_triangle(int size)
+{
+	int i, p;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	else
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (p = 0; p <= i; p++)
+					_putchar('#');
+				_putchar(' ');
+			_putchar('\n');
+		}
+	}
+}
