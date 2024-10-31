e59acbc92d79959481048587b156749bbdf9afd3
elifigueroa9725 <elijfigueroaperez@gmail.com>
2024-10-30 20:30:35 -0400

    fixed command prompt

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,23 +10,25 @@ int main(void)
 {
 	int i = 1;
 
-	while (1 <= 100)
-	  i++;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("Fizz Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
 
-	if (i % 3 == 0)
-		printf("Fizz");
-	
-	else if (i % 5 == 0)
-		printf("Buzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 
-	else if (i % 3 == 0 && i % 5 == 0)
-		printf("Fizz Buzz");
+		else
+			printf("%d", i);
 
-	else
-		printf("%d" , i);
-
-	if (i != 100)
+		if (i < 100)
 		printf(" ");
+	}
+
+	printf("\n");
 
 	return (0);
+
 }
