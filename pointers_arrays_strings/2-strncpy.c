185dcce5a9942c08390a4813b73b319e1b300d89
elifigueroa9725 <elijfigueroaperez@gmail.com>
2024-11-15 16:26:33 -0400

    added exercise 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copy a string.
+ * @dest: pointer.
+ * @src: pointer.
+ * @n: pointer.
+ * Return: value 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
