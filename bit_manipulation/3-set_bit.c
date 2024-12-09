346c06bb990b73f03c2a14a9202153c70493e49d
yilipr <elijfigueroaperez@gmail.com>
2024-12-08 22:51:39 -0500

    fixed command prompt

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * set_bit - Sets value of bit to 1
- *
- * @n: long int
- *
- * @index: unsigned int
- *
- * Return: 1 if works -1 on failure
+ * set_bit - sets value of a bit to 1 at a given index
+ * @n: decimal number passed by pointer
+ * @index: index position to change, starting from 0
+ * Return: 1 if it worked, -1 if error
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-		if (index < sizeof(n) * 8)
-				{
-							*n = *n | (1 << index);
-									return (1);
-										}
+	unsigned long int p;
 
-			return (-1);
+	if (index > 64)
+		return (-1);
+
+	for (p = 1; index > 0; index--, p *= 2)
+		;
+	*n += p;
+
+	return (1);
 }
