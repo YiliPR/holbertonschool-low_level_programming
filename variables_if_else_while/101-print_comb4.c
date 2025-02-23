b9a5163e61abc2e01317ce8f7933d4eba83d00bd
yilipr <elijfigueroaperez@gmail.com>
2025-02-23 12:17:19 -0500

    o

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -9,25 +9,25 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 8; i++) // First digit
+	for (i = 0; i < 8; i++)
 	{
-		for (j = i + 1; j < 9; j++) // Second digit
+		for (j = i + 1; j < 9; j++)
 		{
-			for (k = j + 1; k < 10; k++) // Third digit
+			for (k = j + 1; k < 10; k++)
 			{
-				putchar(i + '0'); // Print first digit
-				putchar(j + '0'); // Print second digit
-				putchar(k + '0'); // Print third digit
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 
-				if (i == 7 && j == 8 && k == 9)
-					break; // Avoid printing ", " after the last combination
-
-				putchar(','); // Print comma
-				putchar(' '); // Print space
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
 
-	putchar('\n'); // Print newline at the end
+	putchar('\n');
 	return (0);
 }
