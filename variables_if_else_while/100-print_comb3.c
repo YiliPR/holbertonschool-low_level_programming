d8782dd69dbf2e0385f279278fea060a5b177123
yilipr <elijfigueroaperez@gmail.com>
2025-02-23 12:09:46 -0500

    t

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,13 @@ int main(void)
 	{
 		for (j = i + 1; j < 10; j++)
 		{
-			putchar(i / 10 + '0');  
-			putchar(i % 10 + '0');  
-			putchar(',');           
-			putchar(' ');           
-	
+			putchar(i + '0');
+			putchar(j + '0');
 			if (i == 8 && j == 9)
-				continue;
+				break;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
