6a8837682c0a8c1adf3aa1bec4b5bd7ab793f7a0
yilipr <elijfigueroaperez@gmail.com>
2025-02-26 19:52:17 -0500

    101

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 8  // Adjust based on crackme's expected length
+
+/**
+ * main - Generates a random password for 101-crackme
+ * Return: 0 on success
+ */
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1];
+    int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        password[i] = (rand() % 94) + 33;
+    }
+    password[PASSWORD_LENGTH] = '\0';
+
+    printf("%s\n", password);
+    return (0);
+}
